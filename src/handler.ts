c41f26f39f7a0e5edbe74c5f6318f42bda241afd
pullmana8 <pullmana8@gmail.com>
2020-05-06 16:18:23 -0500

    Removed Dazn Lambda Powertools

--- src/handler.ts
+++ src/handler.ts
@@ -1,9 +1,6 @@
 import { APIGatewayProxyHandler } from "aws-lambda/trigger/api-gateway-proxy"
 
-const Log = require("@dazn/lambda-powertools-logger")
-
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
-    Log.info('Processing event: ${event}')
     return {
         statusCode: 200,
         body: JSON.stringify({
